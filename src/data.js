90eaee3950feca942586f4980b2558b1bca78689
gaposx <gonzalo.p@laboratoria.la>
2018-12-12 14:31:50 -0500

    Signature of functions in data.js

--- src/data.js
+++ src/data.js
@@ -1,8 +1,22 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
-  return 'example';
+const filterData = (data, condition) => {
+  console.log(data);
+  var filteredData = [...data];
+  filteredData.pokemon.push("new value");
+  console.log(filteredData);
+  return filteredData;
 };
 
-window.example = example;
+const sortData = (data, sortBy, sortOrder) => {
+  return sortedData;
+};
+
+const computeStats = (data) => {
+  return computedData;
+};
+
+window.filterData = filterData;
+window.sortData = sortData;
+window.computeStats = computeStats;
